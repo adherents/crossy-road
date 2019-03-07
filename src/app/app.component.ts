1cbff3e18eb9b4c6e7a445431751d78b149ec601
Dima Simonov <keldor80@gmail.com>
2019-03-07 13:09:57 +0200

    added pixijs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Sprite } from 'pixi.js';
+declare const PIXI: typeof import('pixi.js');
 
 @Component({
   selector: 'cr-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'crossy-road';
+export class AppComponent implements OnInit {
+  @ViewChild('pixiContainer') pixiContainer;
+  public pApp: any; // pixi App
+  ngOnInit() {
+    this.pApp = new PIXI.Application({
+      width: 400,
+      height: 600,
+      backgroundColor: 0x1099bb
+    });
+    this.pixiContainer.nativeElement.appendChild(this.pApp.view);
+  }
 }
