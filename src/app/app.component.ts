ee5bf1b9b213e9f1410b473146bb63fa0dad5a6f
Dima Simonov <keldor80@gmail.com>
2019-03-09 11:53:47 +0200

    added proper images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Sprite } from 'pixi.js';
 declare const PIXI: typeof import('pixi.js');
-
+declare function require(path: string);
 @Component({
   selector: 'cr-root',
   templateUrl: './app.component.html',
@@ -9,13 +9,25 @@ declare const PIXI: typeof import('pixi.js');
 })
 export class AppComponent implements OnInit {
   @ViewChild('pixiContainer') pixiContainer;
-  public pApp: any; // pixi App
+  public app: any; // pixi App
+  cat = require('../app/img/boy.png');
+
   ngOnInit() {
-    this.pApp = new PIXI.Application({
-      width: 400,
+    this.app = new PIXI.Application({
+      width: 600,
       height: 600,
       backgroundColor: 0x1099bb
     });
-    this.pixiContainer.nativeElement.appendChild(this.pApp.view);
+    this.pixiContainer.nativeElement.appendChild(this.app.view);
+
+    const bunny: Sprite = PIXI.Sprite.fromImage(this.cat);
+    bunny.anchor.set(0.5);
+    bunny.x = this.app.screen.width / 1.9;
+    bunny.y = this.app.screen.height / 2;
+
+    this.app.stage.addChild(bunny);
+    /* this.app.ticker.add(function(delta) {
+      bunny.rotation += 0.1 * delta;
+    }); */
   }
 }
