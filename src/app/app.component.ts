13968c123afe1d961ff6114e4c9e57846bf614c8
Dima Simonov <keldor80@gmail.com>
2019-03-09 13:10:56 +0200

    added pixi container for grouping sprites

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { Sprite } from 'pixi.js';
 import { images } from './scripts/images';
+import { container } from './scripts/container';
 declare const PIXI: typeof import('pixi.js');
 
 @Component({
@@ -21,11 +22,23 @@ export class AppComponent implements OnInit {
     });
     this.pixiContainer.nativeElement.appendChild(this.app.view);
 
-    const bunny: Sprite = PIXI.Sprite.fromImage(images.water);
-    bunny.anchor.set(0.5);
-    bunny.x = this.app.screen.width / 1.9;
-    bunny.y = this.app.screen.height / 2;
+    const boy: Sprite = PIXI.Sprite.fromImage(images.water);
+    boy.position.set(0, 0);
+    const car: Sprite = PIXI.Sprite.fromImage(images.car);
+    car.position.set(50, 50);
+    const barrel: Sprite = PIXI.Sprite.fromImage(images.barrel);
+    barrel.position.set(100, 100);
 
-    this.app.stage.addChild(bunny);
+    const test = container;
+
+    test.addChild(boy);
+    test.addChild(car);
+    test.addChild(barrel);
+
+    /* bunny.anchor.set(0.5);
+    bunny.x = this.app.screen.width / 1.9;
+    bunny.y = this.app.screen.height / 2; */
+
+    this.app.stage.addChild(test);
   }
 }
---
+++ src/app/scripts/container.ts
@@ -0,0 +1,1 @@
+export const container = new PIXI.Container();
