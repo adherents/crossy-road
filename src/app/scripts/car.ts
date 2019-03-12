5a886fd95e5bcfd09407521fe7aeaee4b65ff878
Dima Simonov <keldor80@gmail.com>
2019-03-12 20:49:37 +0200

    added proper car logic

--- src/app/scripts/car.ts
+++ src/app/scripts/car.ts
@@ -6,6 +6,7 @@ export const carsContainer = new PIXI.Container();
 class Car extends PIXI.Sprite {
   speed: number;
   startPoint: string;
+  road: number;
 
   constructor() {
     super(PIXI.Texture.fromImage(images.car));
@@ -17,63 +18,58 @@ class Car extends PIXI.Sprite {
 }
 
 export const cars: Array<Car> = [];
-const carsLength = lines.roadLines.length;
+const roadsCount = lines.roadLines.length;
 let car: Car;
 
+const leftRoads = [];
+
+function setDirections(roads: number) {
+  for (let i = 0; i < roads; i++) {
+    const directionLuck = Math.random() * 10;
+    if (directionLuck <= 5) {
+      leftRoads.push(i);
+    }
+  }
+  console.log(leftRoads.length);
+}
+setDirections(roadsCount);
+
 function addCars(amount: number) {
-  for (let i = 0; cars.length < amount; i++ ) {
+  for (let i = 0; i < amount; i++ ) {
     car = new Car();
-    car.y = lines.roadLines[i] - 50;
+    car.road = Math.floor(Math.random() * (roadsCount));
+    car.y = lines.roadLines[car.road] - 50;
+    if (leftRoads.indexOf(car.road) >= 0) {
+      car.startPoint = 'left';
+      car.scale.set(1, -1);
+    } else {
+      car.x = 550;
+      car.startPoint = 'right';
+    }
+    carsContainer.addChild(car);
     cars.push(car);
   }
 }
-addCars(carsLength);
 
-let positionY = 50;
-let positionLuck = 0;
-let x = 0;
-function placeCars() {
-  for (x; x <= cars.length - 1; x++ ) {
-    if ( positionY >= 550) {
-      positionY = 50;
-    }
-    positionLuck = Math.random() * 10;
-    if (positionLuck >= 5) {
-      cars[x].x = 550;
-      cars[x].startPoint = 'right';
-    } else {
-      cars[x].startPoint = 'left';
-      cars[x].scale.set(1, -1);
-    }
+function cleanCars() {
+  if (cars.length > carsContainer.children.length) {
+    cars.splice(0, cars.length - carsContainer.children.length);
   }
 }
-placeCars();
 
 export function carsMove(delta: number) {
   cars.forEach(function(item, i, arr) {
     item.startPoint === 'right' ? item.x -= ((item.speed / 4) + delta)
     : item.x += (item.speed / 4) + delta;
-    if (item.x > 600) {
-      carsContainer.removeChild(item);
-    }
-    if (item.x < 0) {
+    if (item.x < 0 || item.x > 600) {
       carsContainer.removeChild(item);
     }
   });
 }
 
-function carsRender() {
-  cars.forEach(function(item, i, arr) {
-    carsContainer.addChild(cars[i]);
-  });
-}
-
-carsRender();
-
-let carsCount = carsLength;
 setInterval(function() {
-  carsCount += carsLength;
-  addCars(carsCount);
-  placeCars();
-  carsRender();
+  addCars(roadsCount);
+  cleanCars();
+  console.log('Cars ' + cars.length);
+  console.log('Sprites ' + carsContainer.children.length);
 }, (Math.floor(Math.random() * 2001) + 1000));
